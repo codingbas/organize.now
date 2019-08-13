59da7577ffe146221a6053c8f4c6dec008622e70
Ubuntu <ubuntu@ip-172-31-86-237.ec2.internal>
2019-08-13 10:23:33 +0000

    installed django forms bootstrap and bootstrap containers

--- finalmilestone/settings.py
+++ finalmilestone/settings.py
@@ -37,8 +37,10 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'accounts',
+    'django_forms_bootstrap',
+    'accounts'
 ]
+  
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
