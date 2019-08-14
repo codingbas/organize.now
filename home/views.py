402030335b07d2f7bcbec96753df588bfbc76dda
Ubuntu <ubuntu@ip-172-31-86-237.ec2.internal>
2019-08-14 09:24:33 +0000

    created new superuser because old one failed

--- home/views.py
+++ home/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect, reverse
 
 # Create your views here.
+def index(request):
+    if request.user.is_authenticated():
+        return redirect(reverse('dashboard'))
+    else:
+        return redirect(reverse('login')) 
