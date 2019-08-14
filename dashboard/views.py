8e899a6286545163f2e33964523ca910177888c4
Ubuntu <ubuntu@ip-172-31-86-237.ec2.internal>
2019-08-14 09:18:03 +0000

    created dashboard.html and set up views.py

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,3 +1,11 @@
-from django.shortcuts import render
-
 # Create your views here.
+
+from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
+
+@login_required()
+def dashboard_view(request):
+
+    return render(request, "dashboard.html")
+
+
