31821ba012a96266c79a42084a02fdb7e474139a
Ubuntu <ubuntu@ip-172-31-86-237.ec2.internal>
2019-08-14 11:41:17 +0000

    add test.py to dashboard and client.py to accounts

---
+++ accounts/client.py
@@ -0,0 +1,19 @@
+from django.test import Client
+from .forms import UserRegistrationForm
+
+"""
+This is used to simulate a logged in user
+"""
+
+class Login:
+    
+    c = Client()
+
+    def setUp(self):
+        
+        user = UserRegistrationForm({"username":"test", "email":"test@test.com", "password1":"access", "password2":"access"})
+        this_user = user.save()
+        logged_in = self.c.login(username='test', password='access')
+        
+        def __str__(self):
+            return self.name
